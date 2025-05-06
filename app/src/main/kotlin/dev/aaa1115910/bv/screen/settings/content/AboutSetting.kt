01d9b70bf30a8860256e2bc0d203ceb9bb06bf01
licheng <licheng@vocalbeats.com>
2025-05-06 17:47:11 +0800

    更新 ReadMe

--- app/src/main/kotlin/dev/aaa1115910/bv/screen/settings/content/AboutSetting.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/screen/settings/content/AboutSetting.kt
@@ -1,18 +1,7 @@
 package dev.aaa1115910.bv.screen.settings.content
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -73,6 +62,9 @@ fun AboutSetting(
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 Text(
+                    text = stringResource(R.string.about_statement)
+                )
+                Text(
                     text = stringResource(
                         R.string.settings_version_current_version,
                         BuildConfig.VERSION_NAME
@@ -95,7 +87,7 @@ fun AboutSetting(
         }
         Text(
             modifier = Modifier.align(Alignment.BottomCenter),
-            text = "https://github.com/aaa1115910/bv"
+            text = "https://github.com/Leelion96/bv"
         )
     }
 
